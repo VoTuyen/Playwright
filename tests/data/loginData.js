c4e3def71c0e90deea785a75745e7b1d0692b002
Vo Tuyen <votuyen310@gmail.com>
2026-03-01 23:37:06 +0700

    Allow 1 data test

--- tests/data/loginData.js
+++ tests/data/loginData.js
@@ -10,14 +10,14 @@ const dataLogins = [
         benefit_phone: 'SGH796959',
         platform: '_w'
     },
-    {
-        phone: '0565123451',
-        client_id: '1aTxvUI1kFfTSuHFDObHkEs21sDTgm8bEUOCJs9a',
-        type: 'login_fpl',
-        otp_code: '999999',
-        benefit_phone: '056***3451',
-        platform: '_a'
-    },
+    // {
+    //     phone: '0565123451',
+    //     client_id: '1aTxvUI1kFfTSuHFDObHkEs21sDTgm8bEUOCJs9a',
+    //     type: 'login_fpl',
+    //     otp_code: '999999',
+    //     benefit_phone: '056***3451',
+    //     platform: '_a'
+    // },
     
 ];
 
